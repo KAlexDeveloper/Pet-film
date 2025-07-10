168b28ddbe1cfe4f1791dfbe766389ba260a8c73
son-dev-a <anderhander4@gmail.com>
2025-07-10 14:11:51 +0300

    Initial Commit

---
+++ MyMovieTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieTest
+//
+//  Created by сонный on 10.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
